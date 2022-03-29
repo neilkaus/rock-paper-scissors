2de231be1245a43596d0c43481cc9e7974ab43de
Neil Kaus <nkaus@uwaterloo.ca>
2022-03-28 20:38:13 -0400

    Add playRound function

--- javascript.js
+++ javascript.js
@@ -3,3 +3,16 @@ function computerPlay(){
     return ["rock", "paper", "scissors"][Math.floor(Math.random()*3)];
 }
 
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === computerSelection){
+        return "It's a tie!";
+    } else if (playerSelection === "rock" && computerSelection === "scissors" ||
+    playerSelection === "paper" && computerSelection === "rock" ||
+    playerSelection === "scissors" && computerSelection === "paper"){
+        return `You win! ${playerSelection} beats ${computerSelection}`
+    } else {
+        return `You loose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
